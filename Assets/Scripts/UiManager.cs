827bc19207d22107fe5bd30495be0e3a6d422dfa
AliaksandraValodzina <shoora.volodina@gmail.com>
2019-01-31 12:21:41 +0300

    31 01 19

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UiManager : MonoBehaviour
 {
     public static UiManager instance;
     public Text scoreText;
+    public GameObject gameOverPanel;
 
     private void Awake()
     {
@@ -26,5 +28,20 @@ public class UiManager : MonoBehaviour
     void Update()
     {
         scoreText.text = ScoreManager.instance.score.ToString();
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().ToString());
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
